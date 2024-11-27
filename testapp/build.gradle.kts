7ce443dea9f8927acc0de8056bd769eccf0d3fe5
Daniel Cano <60793959+dcanogi@users.noreply.github.com>
2024-11-27 23:00:19 +0100

    Update build.gradle.kts

--- testapp/build.gradle.kts
+++ testapp/build.gradle.kts
@@ -3,10 +3,3 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
-dependencies {
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test:runner:1.5.2")
-}
