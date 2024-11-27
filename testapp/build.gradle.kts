27e42930e2ce2b32d19c8accd72c6dd5ac3970ca
dcanogi <dcanogi47@gmail.com>
2024-11-27 22:42:44 +0100

    Update build.gradle.kts

--- testapp/build.gradle.kts
+++ testapp/build.gradle.kts
@@ -3,3 +3,12 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+dependencies {
+    implementation 'androidx.appcompat:appcompat:1.6.1'
+    implementation 'com.google.android.material:material:1.9.0'
+
+    // Dependencias para pruebas
+    androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.0'
+    androidTestImplementation 'androidx.test.ext:junit:1.1.5'
+    androidTestImplementation 'androidx.test:runner:1.5.2'
+}
