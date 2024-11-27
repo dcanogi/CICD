81df25af2eae76a391f13c79b9038ed13dac2b97
dcanogi <dcanogi47@gmail.com>
2024-11-27 23:49:24 +0100

    commit

--- testapp/app/build.gradle.kts
+++ testapp/app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.core.ktx)
+    implementation(libs.androidx.junit.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- testapp/app/src/test/java/com/example/myapplication/test.kt
+++ testapp/app/src/test/java/com/example/myapplication/test.kt
@@ -1,31 +1,37 @@
 package com.example.myapplication
 
-import android.content.Context
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.core.*
 import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
+    private lateinit var navController: NavController
+
     @Before
     fun setUp() {
-        ActivityScenario.launch(MainActivity::class.java)
+        // Se lanza la actividad para tener acceso a sus elementos.
+        ActivityScenario.launch(MainActivity::class.java).use { scenario ->
+            scenario.onActivity { activity ->
+                // Moca el NavController
+                navController = Mockito.mock(NavController::class.java)
+                Navigation.setViewNavController(activity.findViewById(R.id.nav_view), navController)
+            }
+        }
     }
 
     @Test
-    fun testNavigation() {
-        onView(withId(R.id.nav_view)).check(matches(isDisplayed()))
-
-        onView(withId(R.id.navigation_home)).perform(click())
-
-        onView(withId(R.id.navigation_home))
-            .check(matches(isDisplayed()))
+    fun testNavigationSetup() {
+        // Verifica que el NavController esté correctamente configurado
+        verify(navController).navigate(R.id.navigation_home)  // Asegúrate de que la navegación esté configurada correctamente
     }
 }
