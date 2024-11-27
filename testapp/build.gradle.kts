26bf8d8b12325436fbaf41a79c381bb55bdfde66
dcanogi <dcanogi47@gmail.com>
2024-11-27 22:51:51 +0100

    commit

--- testapp/build.gradle.kts
+++ testapp/build.gradle.kts
@@ -4,11 +4,9 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
 }
 dependencies {
-    implementation 'androidx.appcompat:appcompat:1.6.1'
-    implementation 'com.google.android.material:material:1.9.0'
-
-    // Dependencias para pruebas
-    androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.0'
-    androidTestImplementation 'androidx.test.ext:junit:1.1.5'
-    androidTestImplementation 'androidx.test:runner:1.5.2'
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test:runner:1.5.2")
 }
